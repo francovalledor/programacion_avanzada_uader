4763aa2a61680ad2a3673ac0d19424a4db2056b8
Franco Valledor <francovalledor@gmail.com>
2020-09-07 15:12:49 -0300

    URKU PORTFOLIO parcial

---
+++ TPS/urku/app.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const hbs = require("hbs");
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.use('/assets', express.static('assets'));
+app.set('view engine', 'hbs');
+const VIEWS_PATH = __dirname + '/views/'; 
+
+
+const DATOS = {
+  year: new Date().getFullYear(),
+  propietario: 'Juan Perez',  
+}
+
+
+class Seccion {
+  constructor(nombre, ruta, view){
+    this.nombre = nombre;
+    this.ruta = ruta;
+    this.view = VIEWS_PATH + view;
+  }
+}
+
+class Secciones {
+    lista = new Map()
+
+    has(seccion){
+        return this.lista.has(seccion)
+    }
+
+    add(nombre, ruta, view) {
+      if (! this.has(nombre)) {
+        let seccion = new Seccion(nombre, ruta, view);
+        this.lista.set(nombre, seccion);
+      }
+    }
+
+    get_ruta(seccion){
+      return this.lista.get(seccion).ruta;
+    }
+
+    get_view(seccion){
+      return this.lista.get(seccion).view;
+    }
+}
+
+var secciones = new Secciones();
+
+secciones.add('home', '/home', 'home.hbs');
+secciones.add('portfolio', '/portfolio/', 'portfolio.hbs');
+secciones.add('blog', '/blog/', 'blog.hbs');
+secciones.add('about', '/about/', 'about.hbs');
+secciones.add('contact', '/contact/', 'contact.hbs');
+secciones.add('not_found', '', 'not_found.hbs');
+
+
+app.get('/', (req, res) => res.redirect(secciones.get_ruta('home')));
+
+app.get('/:seccion', (req, res) => {
+  if (secciones.has(req.params.seccion)) {
+        return res.render(secciones.get_view(req.params.seccion), DATOS);
+    } else {
+        return res.status(404).render(secciones.get_view('not_found'), DATOS);
+    }
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
